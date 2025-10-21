0cff6bfe1b9886a87beac6155c70ca5d43797ca6
baliberbagiii-spec <bali.berbagiii@gmail.com>
2025-10-22 05:10:53 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // data.js
 
-const destinations = [
+window.destinations = [
   {
     name: "Ubud",
     image: "assets/ubud.jpg",
@@ -15,39 +15,10 @@ const destinations = [
     name: "Nusa Penida",
     image: "assets/nusa-penida.jpg",
     description: "Pulau eksotis dengan pantai biru jernih dan tebing menakjubkan."
-  },
-  {
-    name: "Uluwatu",
-    image: "assets/uluwatu.jpg",
-    description: "Tebing tinggi dengan pemandangan laut selatan dan pertunjukan tari Kecak."
-  }
-  {
-    name: "Ubud",
-    image: "assets/ubud.jpg",
-    description: "Nikmati suasana tenang di tengah sawah dan budaya Bali yang memikat."
-  },
-  {
-    name: "Tanah Lot",
-    image: "assets/tanah-lot.jpg",
-    description: "Pura di atas batu karang dengan pemandangan sunset yang spektakuler."
-  },
-  {
-    name: "Nusa Penida",
-    image: "assets/nusa-penida.jpg",
-    description: "Pulau eksotis dengan pantai biru jernih dan tebing menakjubkan."
-  },
-  {
-    name: "Uluwatu",
-    image: "assets/uluwatu.jpg",
-    description: "Tebing tinggi dengan pemandangan laut selatan dan pertunjukan tari Kecak."
   }
 ];
 
-
-
-
-// --- Data Paket Wisata ---
-const packages = [
+window.packages = [
   {
     title: "Paket Honeymoon",
     image: "assets/honeymoon.jpg",
@@ -59,17 +30,5 @@ const packages = [
     image: "assets/adventure.jpg",
     price: "Rp 3.800.000 / orang",
     description: "Rasakan adrenalin dengan rafting, trekking, dan wisata alam di pedalaman Bali."
-  },
-  {
-    title: "Paket Keluarga",
-    image: "assets/family.jpg",
-    price: "Rp 4.200.000 / keluarga",
-    description: "Liburan menyenangkan bersama keluarga dengan aktivitas aman dan seru."
-  },
-  {
-    title: "Paket Eksklusif",
-    image: "assets/exclusive.jpg",
-    price: "Rp 9.000.000 / orang",
-    description: "Layanan VIP dengan transportasi pribadi, spa mewah, dan private tour guide."
   }
 ];
