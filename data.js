6faf6c82a6ecb55f24c3732c9ef87285ae0f467e
baliberbagiii-spec <bali.berbagiii@gmail.com>
2025-10-22 05:37:09 +0700

    Update data.js

--- data.js
+++ data.js
@@ -3,17 +3,17 @@
 window.destinations = [
   {
     name: "Ubud",
-    image: "assets/ubud.jpg",
+    image: "https://plus.unsplash.com/premium_photo-1669317566483-d3327adeda10?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2062",
     description: "Nikmati suasana tenang di tengah sawah dan budaya Bali yang memikat."
   },
   {
     name: "Tanah Lot",
-    image: "assets/tanah-lot.jpg",
+    image: "https://images.unsplash.com/photo-1624935851312-845758a99160?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1742",
     description: "Pura di atas batu karang dengan pemandangan sunset yang spektakuler."
   },
   {
     name: "Nusa Penida",
-    image: "assets/nusa-penida.jpg",
+    image: "https://images.unsplash.com/photo-1604560929658-bbc3c2ba6a36?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1546",
     description: "Pulau eksotis dengan pantai biru jernih dan tebing menakjubkan."
   }
 ];
@@ -21,14 +21,15 @@ window.destinations = [
 window.packages = [
   {
     title: "Paket Honeymoon",
-    image: "assets/honeymoon.jpg",
+    image: "https://plus.unsplash.com/premium_photo-1678303396234-4180231353df?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1740",
     price: "Rp 5.500.000 / pasangan",
     description: "Nikmati bulan madu romantis di Bali dengan akomodasi mewah dan pemandangan menawan."
   },
   {
     title: "Paket Petualangan",
-    image: "assets/adventure.jpg",
+    image: "https://images.unsplash.com/photo-1636549882106-494b8efa2f03?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1740",
     price: "Rp 3.800.000 / orang",
     description: "Rasakan adrenalin dengan rafting, trekking, dan wisata alam di pedalaman Bali."
   }
 ];
+
